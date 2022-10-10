ce6561d8ef4f07c1ae61bc7461945869f8e20060
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2022-10-10 17:39:40 -0300

    ✨ added shortcode that lists retirees

--- bope/app/public/wp-config.php
+++ bope/app/public/wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The base configuration for WordPress
  *
@@ -11,7 +12,6 @@
  * * Database settings
  * * Secret keys
  * * Database table prefix
- * * Localized language
  * * ABSPATH
  *
  * @link https://wordpress.org/support/article/editing-wp-config-php/
@@ -21,19 +21,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+define( 'DB_NAME', 'exbras19_wp43' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'exbras19_wp43' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'F34)3S)pfQ' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -49,7 +49,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-
+define( 'AUTH_KEY',         'zadtd7khkjz0l07owpxzxduxahb29430qwt76wveohxv9lbq596jjxf4yrcrg88w' );
+define( 'SECURE_AUTH_KEY',  '21juswsrapbekgcierliyhsvyrvmknpnw5qcn9mjld3cpmvaaxampddtuwbz0qcn' );
+define( 'LOGGED_IN_KEY',    'scsys8wjkhp8sxjvkytxdrpwemfo2m7ti6yshopwlmkgfibzdod79zqarms48pai' );
+define( 'NONCE_KEY',        'a95qppvdea12zm7qnm6dvwrwf1gmno9hxwiaad9fvynb2khitz6ywrgqdqmtcxo9' );
+define( 'AUTH_SALT',        'giqcsqhudg7vfewuizssfdrelizi8frhjufjabckuufqhva2dzgfhkzatplbjaev' );
+define( 'SECURE_AUTH_SALT', 'trhqwy2fq4ooysbp0wh1bkykm07yzoqlrabg1klyf6s5ltyh2wgzpnzvxzsxevwi' );
+define( 'LOGGED_IN_SALT',   'vlxmzlsvuo1k39b82meae5m95bm6iwo7kkbhqbn17ajozn1rnqhb2vggu6zvact1' );
+define( 'NONCE_SALT',       'fc18wghw5ls0xkwd5ds5uhchdz0youj3ila7daionspxfka9eg2x2rmea1l8oew7' );
 
 /**#@-*/
 
@@ -59,7 +66,7 @@ define( 'DB_COLLATE', '' );
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix = 'wp_';
+$table_prefix = 'wphv_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -75,7 +82,6 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-
 /* Add any custom values between this line and the "stop editing" line. */
 
 
@@ -89,3 +95,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+
+//[aposentados]
+function aposentados_func(){
+  $html = '';
+
+  $url = "https://bopehabbo.net/api/v2/aposentados.php";
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $api = json_decode(curl_exec($ch), true);
+
+  $html .= '<p>';
+    foreach($api as $item) {
+      $html .= '<img title="Ex-'.$item['pat_nome'].', '.$item['nome'].'" src="https://www.habbo.com.br/habbo-imaging/avatarimage?img_format=png&amp;user='.$item['nome'].'&amp;direction=2&amp;head_direction=2&amp;size=m&amp;action=std" alt="" width="64" height="110">';
+    }
+  $html .= '</p>';
+
+  return $html;
+}
+add_shortcode( 'aposentados', 'aposentados_func' );
